875d3fb8ee4f9546456a96558d1ed4f11691f657
hyl <17803000829@163.com>
2024-01-31 21:42:16 +0800

    扩展滑块

---
+++ src/elements/Slider/index.js
@@ -0,0 +1,28 @@
+import { ElSlider } from 'element-plus'
+import { linkageAttr, basic, props } from '../commonAttr'
+
+export default {
+  name: '滑块',
+  component: ElSlider,
+  icon: 'slider',
+  type: 'basic',
+  order: 16,
+  initialValues: { label: '滑块', component: 'Slider' },
+  attr: [
+    ...basic,
+    { label: '初始值', component: 'InputNumber', name: 'initialValue' },
+    {
+      label: 'props',
+      component: 'ItemGroup',
+      name: 'props',
+      children: [
+        ...props,
+        { label: '开启选择范围', component: 'Switch', name: 'range' },
+        { label: '步长', component: 'InputNumber', name: 'step', initialValue: 1 },
+        { label: '最小值', component: 'InputNumber', name: 'min', initialValue: 0 },
+        { label: '最大值', component: 'InputNumber', name: 'max', initialValue: 100 }
+      ]
+    },
+    ...linkageAttr
+  ]
+}
