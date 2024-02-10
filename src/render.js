8fb2d8556db102444aa2704474406c1b01253a10
hyl <3307578337@qq.com>
2024-02-10 12:54:33 +0800

    优化

--- src/render.js
+++ src/render.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import router from './router'
 // import VueFormCraft from '../dist/vue-form-craft.mjs'
-import VueFormCraft from './release'
+import VueFormCraft from './release/index'
 import { request } from '@/utils'
+import { MdPreview, MdCatalog, MdEditor } from 'md-editor-v3'
+import 'md-editor-v3/lib/style.css'
 // import '@arco-design/web-vue/dist/arco.css'
 // import { Switch, Input, Textarea, InputNumber } from '@arco-design/web-vue'
 // import 'ant-design-vue/dist/reset.css'
 // import { Switch, Input, Textarea, InputNumber } from 'ant-design-vue'
+
+const components = [MdPreview, MdCatalog, MdEditor] // 全局组件列表
 
 const render = (component, dom) => {
   const app = createApp(component)
@@ -26,30 +30,34 @@ const render = (component, dom) => {
       return JSON.parse(data[0].formSchema)
     }
     // icon: { component: IconRender, propKey: 'name' }
-    //   customElements: {
-    //     Input: {
-    //       component: Input,
-    //       modelName: 'value'
-    //     },
-    //     Switch: {
-    //       component: Switch,
-    //       modelName: 'checked',
-    //       propAttrs: [
-    //         { label: '打开时描述', component: 'Input', name: 'checked-text' },
-    //         { label: '关闭时描述', component: 'Input', name: 'unchecked-text' }
-    //       ]
-    //     },
-    //     Textarea: {
-    //       component: Textarea,
-    //       modelName: 'value'
-    //     },
-    //     InputNumber: {
-    //       component: InputNumber,
-    //       modelName: 'value'
-    //     }
+    // customElements: {
+    //   Input: {
+    //     component: Input,
+    //     modelName: 'value'
+    //   },
+    //   Switch: {
+    //     component: Switch,
+    //     modelName: 'checked',
+    //     propAttrs: [
+    //       { label: '打开时描述', component: 'Input', name: 'checked-text' },
+    //       { label: '关闭时描述', component: 'Input', name: 'unchecked-text' }
+    //     ]
+    //   },
+    //   Textarea: {
+    //     component: Textarea,
+    //     modelName: 'value'
+    //   },
+    //   InputNumber: {
+    //     component: InputNumber,
+    //     modelName: 'value'
     //   }
+    // }
   })
 
+  // 注册组件
+  components.forEach((component) => {
+    app.component(component.name, component)
+  })
   app.mount(dom)
 }
 
