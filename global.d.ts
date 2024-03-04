794bdc786feef28d07f2677d3267ec3745197191
hyl <3307578337@qq.com>
2024-03-04 14:14:02 +0800

    项目升级整合ts

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,7 @@
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     SchemaForm: (typeof import('vue-form-craft'))['SchemaForm']
+    FormDesign: (typeof import('vue-form-craft'))['FormDesign']
   }
 }
 
--- src/config/globalComponent.d.ts
+++ src/config/globalComponent.d.ts
@@ -1,8 +1,10 @@
 import SchemaForm from '@/components/SchemaForm.vue'
+import FormDesign from '@/components/FormDesign/index.vue'
 
 declare module 'vue' {
   export interface GlobalComponents {
     SchemaForm: typeof SchemaForm
+    FormDesign: typeof FormDesign
   }
 }
 
