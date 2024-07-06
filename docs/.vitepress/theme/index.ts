62cc28f584647dc8ca6e524a28896f7e8d3db627
hyl <3307378337@qq.com>
2024-07-06 19:04:21 +0800

    new docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,15 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-// @ts-ignore
-import VueFormCraft from '@/release/index'
 import { request } from '@/utils'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
-    // 注册自定义全局组件
-    // @ts-ignore
-    app.use(VueFormCraft, { request })
+  async enhanceApp({ app }) {
+    if (!import.meta.env.SSR) {
+      const { default: VueFormCraft } = await import('@/release/index')
+      // @ts-ignore
+      app.use(VueFormCraft, { request })
+    }
   }
 } satisfies Theme
