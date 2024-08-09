baadfc78fcfe67367c5a1fea1cbb58097c2eb608
huyilin <v_huyilin@baidu.com>
2024-08-09 17:38:48 +0800

    项目瘦身

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import App from './App.vue'
-import render from './render'
-
-render(App, '#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import libCss from 'vite-plugin-libcss'
 import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
-
-  const config = {
+export default defineConfig({
     base: '/vue-form-craft/',
     plugins: [
       vue(),
       dts({
         outDir: './dist/types',
         rollupTypes: false
-        // include: ['src/release/**/*', 'src/components/**/*']
       }),
       vueJsx(),
       viteCommonjs(),
@@ -29,36 +24,23 @@ export default defineConfig(({ mode }) => {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    server: {
-      open: true
-    },
-    assetsInclude: ['**/*.md']
-  }
-
-  //发布到npm的打包配置
-  if (env.VITE_App_RELEASE) {
-    return {
-      ...config,
-      build: {
-        lib: {
-          entry: fileURLToPath(new URL('./src/release/index.ts', import.meta.url)), //指定组件编译入口文件
-          name: 'vue-form-craft', // 包名
-          fileName: 'vue-form-craft' // 打包文件名
-        },
-        rollupOptions: {
-          // 确保外部化处理那些你不想打包进库的依赖
-          external: ['vue'],
-          output: {
-            // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-            globals: {
-              vue: 'Vue'
-            }
+    build: {
+      lib: {
+        entry: fileURLToPath(new URL('./src/release/index.ts', import.meta.url)), //指定组件编译入口文件
+        name: 'vue-form-craft', // 包名
+        fileName: 'vue-form-craft' // 打包文件名
+      },
+      rollupOptions: {
+        // 确保外部化处理那些你不想打包进库的依赖
+        external: ['vue'],
+        output: {
+          // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+          globals: {
+            vue: 'Vue'
           }
         }
-        // sourcemap: true
       }
+      // sourcemap: true
     }
   }
-
-  return config
-})
+)
