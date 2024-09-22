eb1f7603fd7caa425bb0b9520f7d079b5c733cc0
hyl <3307378337@qq.com>
2024-09-22 22:09:16 +0800

    项目重构4.0

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
+      external: ['vue','element-plus'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
