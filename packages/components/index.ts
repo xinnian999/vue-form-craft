7d372671609b36d554d8445f3b1e37d4ed3c38ff
hyl <3307378337@qq.com>
2024-11-16 17:33:30 +0800

    feat 优化目录

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Disabled } from './Disabled.vue'
 export { default as CodeHighLight } from './CodeHighLight.vue'
 export { default as FormItemGroup } from './FormItemGroup/index.vue'
+export { default as FormItem } from './FormItemGroup/FormItem.vue'
--- play/src/main.ts
+++ play/src/main.ts
@@ -4,11 +4,14 @@ import router from './router'
 import VueFormCraft from '@vue-form-craft/entry'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(VueFormCraft)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn
+})
 
 app.mount('#app')
