ee461995746307067645a716dc34cfe822dbbf83
v_huyilin <v_huyilin@baidu.com>
2024-11-21 18:18:51 +0800

    feat 对象容器

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Disabled } from './Disabled.vue'
 export { default as CodeHighLight } from './CodeHighLight.vue'
+export { default as CanvasGroup } from './FormItemGroup/CanvasGroup.vue'
 export { default as FormItemGroup } from './FormItemGroup/index.vue'
 export { default as FormItem } from './FormItemGroup/FormItem.vue'
