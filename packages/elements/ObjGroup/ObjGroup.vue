d0aec265988020cd382113f297f1a99e86b07b30
hyl <3307378337@qq.com>
2024-11-25 22:36:00 +0800

    feat  对象容器name穿透

--- packages/elements/ObjGroup/ObjGroup.vue
+++ packages/elements/ObjGroup/ObjGroup.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="vfc-ObjGroup">
-    <CanvasGroup
-      v-if="formInstance.design"
-      :list="children"
-    />
+    <CanvasGroup v-if="formInstance.design" :list="children" />
     <div v-else class="vfc-ObjGroup-render">
       <FormItem
         v-for="item in children"
         :key="item.name"
         v-bind="item"
-        :name="`${name}.${item.name}`"
+        :name="`${base}.${item.name}`"
       />
     </div>
   </div>
@@ -19,13 +16,21 @@
 import { CanvasGroup, FormItem } from '@vue-form-craft/components'
 import type { FormItemType } from '@vue-form-craft/types'
 import { useFormInstance } from '@vue-form-craft/hooks'
+import { computed, inject, provide } from 'vue'
 
-defineProps<{
+const props = defineProps<{
   children: FormItemType[]
   name: string
 }>()
 
 const formInstance = useFormInstance()
+
+const objGroupBase = inject('$objGroupBase')
+
+
+const base = computed(() => (objGroupBase ? `${objGroupBase}.${props.name}` : props.name))
+
+provide('$objGroupBase', base.value)
 </script>
 
 <style lang="scss">
