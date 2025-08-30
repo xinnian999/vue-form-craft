e663c46c386d02dc407f3843e11fc5e853d1b0d4
hyl <3307378337@qq.com>
2025-08-30 18:26:37 +0800

    feat ai

--- packages/element-plus/src/components/AIGenerate.vue
+++ packages/element-plus/src/components/AIGenerate.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="ai-generate">
-    <el-input v-model="input" size="large">
+  <div :class="ns('ai-generate')">
+    <el-input v-model="input" size="large" placeholder="请输入表单需求，例如：生成一个登陆表单">
       <template #append>
         <el-button @click="handleGenerate" :loading="loading">AI生成</el-button>
       </template>
@@ -11,9 +11,11 @@
 <script setup lang="ts">
 import axios from 'axios'
 import { ref } from 'vue'
-import { useDesignInstance } from '@form-magic/core'
+import { tools, useDesignInstance } from '@form-magic/core'
 
-const input = ref('生成一个登陆表单')
+const { ns } = tools
+
+const input = ref('')
 
 const loading = ref(false)
 
@@ -23,7 +25,6 @@ const handleGenerate = async () => {
   loading.value = true
   const res = await axios.post('/ai/generateForm', { input: input.value })
   const data = res.data.data
-  // console.log(data)
   loading.value = false
 
   const match = data.match(/```json\s*([\s\S]*?)```/)
@@ -33,39 +34,5 @@ const handleGenerate = async () => {
     console.log('json', json)
     designInstance.updateSchema(json)
   }
-  // designInstance.updateSchema({
-  //   labelWidth: 150,
-  //   labelAlign: 'right',
-  //   size: 'default',
-  //   scrollToError: true,
-  //   items: [
-  //     {
-  //       component: 'Input',
-  //       designKey: 'design-l6dB',
-  //       name: 'form-LXJ2',
-  //       label: '单行文本',
-  //       props: {
-  //         placeholder: '请输入文本'
-  //       }
-  //     }
-  //   ]
-  // })
 }
 </script>
-
-<style scoped lang="scss">
-.ai-generate {
-  position: absolute;
-  bottom: 50px;
-  left: 50%;
-  transform: translateX(-50%);
-  display: flex;
-  gap: 10px;
-  width: 500px;
-  z-index: 9999;
-  background: #fff;
-  padding: 15px;
-  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-  border-radius: 5px;
-}
-</style>
