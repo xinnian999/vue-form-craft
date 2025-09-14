a998e99f27d77f98c91d07743bfc3a380c831e66
hyl <3307378337@qq.com>
2025-09-14 15:02:12 +0800

    feat

--- play/element-plus/src/main.ts
+++ play/element-plus/src/main.ts
@@ -5,7 +5,6 @@ import VueFormCraft from 'vue-form-craft/dev'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import ElementPlusX from 'vue-element-plus-x'
 
 const app = createApp(App)
 
@@ -14,6 +13,5 @@ app.use(VueFormCraft)
 app.use(ElementPlus, {
   locale: zhCn
 })
-app.use(ElementPlusX)
 
 app.mount('#app')
