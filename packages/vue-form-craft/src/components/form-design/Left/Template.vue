5ab76ee02b5a3275a458c53f73018e6639565211
hyl <3307378337@qq.com>
2025-09-24 22:02:44 +0800

    feat: 添加表单模板列表的标题和描述样式

--- packages/vue-form-craft/src/components/form-design/Left/Template.vue
+++ packages/vue-form-craft/src/components/form-design/Left/Template.vue
@@ -1,9 +1,13 @@
 <template>
-  <el-space wrap alignment="center">
-    <el-button :key="name" v-for="{ name, schema } in templates" @click="useTemplate(schema)">
-      {{ name }}
-    </el-button>
-  </el-space>
+  <div class="template-list">
+    <div class="title">表单模板</div>
+    <div class="description">用于管理和切换表单模板，方便在不同业务场景中快速切换和配置表单。</div>
+    <el-space wrap alignment="center">
+      <el-button :key="name" v-for="{ name, schema } in templates" @click="useTemplate(schema)">
+        {{ name }}
+      </el-button>
+    </el-space>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -18,5 +22,3 @@ const useTemplate = (schema: any) => {
   designInstance.updateSchema(schema)
 }
 </script>
-
-<style></style>
