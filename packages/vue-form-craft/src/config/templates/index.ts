f598ed692a4a67e2dc1de583d8794ae71f12cbff
Elin <3307578337@qq.com>
2025-09-25 21:57:50 +0800

    docs: 新增表单设计器规范文档并优化模板示例

--- packages/vue-form-craft/src/config/templates/index.ts
+++ packages/vue-form-craft/src/config/templates/index.ts
@@ -1,42 +1,27 @@
 import bugGood from './bugGood'
 import satisfaction from './satisfaction'
-import type { TemplateData } from '@vue-form-craft/core'
-import login from './login'
-import register from './register'
 import formList from './formList'
-import linkage1 from './linkage1'
-import linkage2 from './linkage2'
 import valueLinkage from './valueLinkage'
-import linkage4 from './linkage4'
-import MostTest from './300Test'
-import nest from './nest'
-
+import register from './register'
 
 export default [
   {
     label: '常见表单',
     children: [
-      { label: '登陆账号', schema: login, description: '简单的登陆表单，包含账号密码，验证码等。快速实现登录功能' },
-      { label: '注册账号', schema: register, description: '简单的注册表单，包含账号密码，验证码等' },
-      { label: '商品购买', schema: bugGood, id: 'bugGood' },
-      { label: '用户满意度调查', schema: satisfaction, id: 'satisfaction' },
+      {
+        label: '注册账号',
+        schema: register,
+        description: '常见的《注册账号》表单，包含了用户名、密码、确认密码等字段。 且【密码】和【确认密码】之间具备校验逻辑。手机号、邮箱都具备格式校验。'
+      },
     ]
   },
   {
-    label: '联动案例',
+    label: '表单联动案例',
     children: [
-      { label: '联动案例1', schema: linkage1, id: 'linkage1' },
-      { label: '联动案例2', schema: linkage2, id: 'linkage2' },
-      { label: '联动案例4', schema: linkage4, id: 'linkage4' },
-      { label: '自增组件', schema: formList, id: 'formList' },
-      { label: '值联动', schema: valueLinkage, id: 'valueLinkage' },
-    ]
-  },
-  {
-    label: '测试案例',
-    children: [
-      { label: '大量表单项测试', schema: MostTest, id: 'MostTest' },
-      { label: '容器嵌套', schema: nest, id: '容器嵌套' },
+      { label: '用户满意度调查', schema: satisfaction, description: '简单的用户满意度调查表单。联动效果：当用户满意度程度低时，可以输入改进建议' },
+      { label: '商品购买', schema: bugGood, description: '商品购买表单。联动效果：选择商品后，将商品金额展示出来' },
+      { label: '自增组件联动', schema: formList, description: '自增组件联动表单' },
+      { label: '数据联动', schema: valueLinkage, description: '数据联动：当字段A输入时，字段B和C的数据会随之改变' },
     ]
   },
 ]
