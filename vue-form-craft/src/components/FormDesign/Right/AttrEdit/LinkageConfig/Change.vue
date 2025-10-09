f395364a0f5b30f64da72966be9d69fd94803d11
v_huyilin <v_huyilin@baidu.com>
2025-10-09 11:27:51 +0800

    refactor: 重构表单联动配置界面并优化帮助文档

--- vue-form-craft/src/components/FormDesign/Right/AttrEdit/LinkageConfig/Change.vue
+++ vue-form-craft/src/components/FormDesign/Right/AttrEdit/LinkageConfig/Change.vue
@@ -1,15 +1,52 @@
 <template>
   <el-dialog destroy-on-close v-model="visible" top="3vh" title="数据联动">
-    <FormRender v-model="designInstance.current!" :schema="linkageSchema()" />
+    <FormRender v-model="designInstance.current!" :schema="schema" />
   </el-dialog>
 </template>
 
 <script setup lang="ts">
 import { FormRender } from '@/components'
 import { useDesignInstance } from '@/hooks'
-import linkageSchema from './changeSchema'
+import type { FormSchema } from '@/types'
 
 const designInstance = useDesignInstance()
 
 const visible = defineModel<boolean>()
+
+const schema: FormSchema = {
+  labelWidth: 100,
+  labelAlign: 'top',
+  items: [
+    {
+      hideLabel: true,
+      name: 'change',
+      component: 'FormList',
+      children: [
+        {
+          label: '目标字段',
+          name: 'target',
+          component: 'Input',
+          required: true,
+          help: '目标字段的name'
+        },
+        {
+          label: '触发条件',
+          name: 'condition',
+          component: 'Input',
+          help: '可选<br/>如果不设置，默认会一直触发联动。 <br/>如果设置，输入一个JS表达式，当表达式结果为true时才触发联动。'
+        },
+        {
+          label: '新数据',
+          name: 'value',
+          component: 'Input',
+          help: '可选<br/>如果不填，则直接清空目标字段的数据'
+        }
+      ],
+      props: {
+        mode: 'card',
+        title: '数据联动'
+      }
+    }
+  ]
+}
 </script>
