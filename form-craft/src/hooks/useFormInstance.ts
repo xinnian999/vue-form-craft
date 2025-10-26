8ed93cd80364af3e7b4fe33ebf83799a466cfd88
hyl <3307378337@qq.com>
2025-10-26 21:26:09 +0800

    refactor: 移除useFormInstance中的mock实例，改为强制注入

--- form-craft/src/hooks/useFormInstance.ts
+++ form-craft/src/hooks/useFormInstance.ts
@@ -1,25 +1,9 @@
-import { inject, reactive } from 'vue'
+import { inject } from 'vue'
 import { $formInstance } from '@/symbol'
 
 // 返回一个响应式的Proxy，所以不可解构使用
 const useFormInstance = () => {
-  const mockInstance = reactive({
-    schema: { items: [] },
-    schemaContext: {},
-    design: false,
-    read: false,
-    formValues: {},
-    selectData: {},
-    initialValues: {},
-    context: {},
-    updateSelectData: () => { },
-    updateInitialValues: () => { },
-    validate: () => Promise.reject(),
-    resetFields: () => { },
-    submit: () => { }
-  })
-
-  const instance = inject($formInstance, mockInstance)
+  const instance = inject($formInstance)!
 
   return instance
 }
