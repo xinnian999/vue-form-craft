ad057aa40b0c98914377addc95cfc0a98842c3c5
v_huyilin <v_huyilin@baidu.com>
2025-10-27 14:45:55 +0800

    style: 格式化useChildrenModel代码并优化注释结构

--- form-craft/src/hooks/useChildrenModel.ts
+++ form-craft/src/hooks/useChildrenModel.ts
@@ -1,36 +1,34 @@
-import { computed } from "vue"
-import type { FormItemType } from "@/types"
-import { useFormInstance, useDesignInstance } from "@/hooks"
+import { computed } from 'vue'
+import { useDesignInstance, useFormInstance } from '@/hooks'
+import type { FormItemType } from '@/types'
 
-// 容器组件专用，提供双向绑定的children。在设计器模式下，允许被修改。
-const useChildrenModel = (props: {
-    children?: FormItemType[]
-    designKey: string
-}) => {
-    const formInstance = useFormInstance()
+// 容器组件专用，提供双向绑定的children。
+// 在设计器模式下，允许通过designKey修改children。
+const useChildrenModel = (props: { children?: FormItemType[]; designKey: string }) => {
+  const formInstance = useFormInstance()
 
-    const designInstance = useDesignInstance()
+  const designInstance = useDesignInstance()
 
-    const childrenModel = computed({
-        get() {
-            return props.children || []
-        },
-        set(value) {
-            if (formInstance.design) {
-                if (props.designKey === 'root') {
-                    designInstance!.updateNodeByKey('root', {
-                        items: value
-                    })
-                } else {
-                    designInstance!.updateNodeByKey(props.designKey, {
-                        children: value
-                    })
-                }
-            }
+  const childrenModel = computed({
+    get() {
+      return props.children || []
+    },
+    set(value) {
+      if (formInstance.design) {
+        if (props.designKey === 'root') {
+          designInstance!.updateNodeByKey('root', {
+            items: value
+          })
+        } else {
+          designInstance!.updateNodeByKey(props.designKey, {
+            children: value
+          })
         }
-    })
+      }
+    }
+  })
 
-    return childrenModel
+  return childrenModel
 }
 
 export default useChildrenModel
